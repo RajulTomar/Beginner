423d904b0f777b58cb6ac0af122b38b22a688bba
RajulTomar <48259662+RajulTomar@users.noreply.github.com>
2019-03-06 22:46:39 +0530

    Create guvievenodd.java

---
+++ guvievenodd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Guvievenodd
+{
+	public static void main(String args[])
+	{
+	Scanner sc =new Scanner(System.in);
+		int number;
+		System.out.println("enter the number");
+		number=sc.nextInt();
+	
+		if (number%2==0)
+		System.out.println("number is even");
+	
+		else  
+		System.out.println("number is odd");
+	}
+}
